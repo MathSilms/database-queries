3c216e67d3874fd4fb5ec20d77b51c8ab7f4b284
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2022-10-08 18:22:42 -0300

    UsersRepository testes

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,33 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
+
+    const user = await this.repository.findOne({ where: { id: user_id }, relations: ["games"] })
+    
+    if (!user) throw new Error()
+
+    return user
     // Complete usando ORM
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(`
+    SELECT * FROM users AS u ORDER BY u.first_name ASC
+    `);
+
+    // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+
+    return await this.repository.query(`
+    SELECT * FROM users WHERE LOWER(last_name) =LOWER('${last_name}') AND LOWER(first_name) =LOWER('${first_name}')
+    `);
+
+    // Complete usando raw query
+
   }
 }
